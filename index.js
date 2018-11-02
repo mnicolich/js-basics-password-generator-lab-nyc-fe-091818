b35de46284459fc2886ed998db65e31ba758c036
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-02 00:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ window.addEventListener("load", () => {
   //start your code here
   
   //1. figure out how long our password should break
-  document.addEventListener('submit', (event) =>
+  document.addEventListener('submit', (event) => {
+    event.preventDefault()
+  }
   
   
   //2. we need to make a password from the ground up 
