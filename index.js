173c4efe6f2e1edeb42379cd3b0460458a34d210
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-02 00:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  //1. figure out how long our password should break
+  
+  
+  
+  //2. we need to make a password from the ground up 
+  
+  //3. we need to keep track fo requirments 
+  
+  
+  
 });
